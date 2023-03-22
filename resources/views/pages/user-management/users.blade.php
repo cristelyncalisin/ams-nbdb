be3b6a05e82e0f1e78efc41a6ece867e1250ee76
Eric Marvin Baste <marvinbaste000@gmail.com>
2023-03-22 19:25:42 +0800

    User Management

--- resources/views/pages/user-management/users.blade.php
+++ resources/views/pages/user-management/users.blade.php
@@ -4,42 +4,59 @@
 
 @section('content')
 <h4 class="fw-bold py-3 mb-4">
-  <span class="text-muted fw-light">User Management /</span> List of Users
+    <span class="text-muted fw-light">User Management /</span> List of Users
 </h4>
 
 <!-- Responsive Table -->
 <div class="card">
-  <div class="table-responsive text-nowrap">
-    <table class="table">
-      <thead>
-        <tr class="text-nowrap">
-          <th>ID #</th>
-          <th>Full Name</th>
-          <th>Email</th>
-          <th>Username</th>
-          <th>Role</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-            @forelse($users as $user)
-                <tr>
-                    <th scope="row">{{ $user->id }}</th>
-                    <td>{{ $user->last_name . ', ' . $user->first_name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->username }}</td>
-                    <td>{{ $user->role }}</td>
-                    <td>
-                        <input class="form-check-input" type="checkbox" disabled {{ $user->is_active ? 'checked' : '' }}>
-                    </td>
+    <div class="table-responsive text-nowrap">
+        <table class="table">
+            <thead>
+                <tr class="text-nowrap">
+                    <th>#</th>
+                    <th>User</th>
+                    <th>Username</th>
+                    <th>Role</th>
+                    <th class="text-center">Is Active?</th>
+                    <th>Date Created</th>
                 </tr>
-                
-            @empty
-                <p>No users</p>
-            @endforelse
-      </tbody>
-    </table>
-  </div>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{ $loop->iteration }}</th>
+                        <td>
+                            <div class="d-flex justify-content-left align-items-center">
+                                <div class="avatar-wrapper">
+                                    <div class="avatar avatar-sm me-3">
+                                        <span class="avatar-initial rounded-circle bg-label-primary">{{ substr($user->last_name, 0, 1) . substr($user->first_name, 0, 1) }}</span>
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <a href="app-user-view-account.html" class="text-body text-truncate">
+                                        <span class="fw-semibold">{{ $user->last_name . ', ' . $user->first_name }}</span>
+                                    </a>
+                                    <small class="text-muted">{{ $user->email }}</small>
+                                </div>
+                            </div>
+                        </td>
+                        <td>{{ '@' . $user->username }}</td>
+                        <td>
+                            <span class="text-truncate d-flex align-items-center">
+                                <span class="badge badge-center rounded-pill bg-label-primary w-px-30 h-px-30 me-2">
+                                    <i class="bx bx-cog bx-xs"></i>
+                                </span>{{ $user->role }}
+                            </span>
+                        </td>
+                        <td class="text-center">
+                            <input class="form-check-input" type="checkbox" disabled {{ $user->is_active ? 'checked' : '' }}>
+                        </td>
+                        <td>@prettyDateFormat($user->date_created)</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 <!--/ Responsive Table -->
 @endsection
